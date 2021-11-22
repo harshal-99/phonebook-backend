eee2727a02c96871140abc2d48ccbed9b8a933fd
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-22 07:08:54 +0530

    Initial commit.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import morgan  from "morgan"
+import cors from "cors"
 
 const app = express()
 
@@ -40,6 +41,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 
 app.get('/api/persons', (req, res) => {
